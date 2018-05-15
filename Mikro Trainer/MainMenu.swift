d439b4e103072287c8f278e52466e3c74a66cf8f
Michael Gutkind <mikegutkind@gmail.com>
2018-05-15 13:46:29 -0400

    Cocoa pods complete. Firebase setup complete. Hello World.

---
+++ Mikro Trainer/MainMenu.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mikro Trainer
+//
+//  Created by Michael Gutkind on 5/15/18.
+//  Copyright © 2018 Michael Gutkind. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("test")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Mikro TrainerTests/Pods-Mikro TrainerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mikro_TrainerTests : NSObject
+@end
+@implementation PodsDummy_Pods_Mikro_TrainerTests
+@end
