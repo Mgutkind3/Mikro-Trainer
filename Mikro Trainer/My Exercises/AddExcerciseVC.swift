7732f2c1f9fa48227dad0b49079a9eee25a0b056
Michael Gutkind <mikegutkind@gmail.com>
2018-05-16 12:45:11 -0400

    Working on addExercise feature. Not fully implimented but working.

---
+++ Mikro Trainer/My Exercises/AddExcerciseVC.swift
@@ -0,0 +1,35 @@
+//
+//  AddExcerciseVC.swift
+//  Mikro Trainer
+//
+//  Created by Michael Gutkind on 5/16/18.
+//  Copyright © 2018 Michael Gutkind. All rights reserved.
+//
+
+import UIKit
+
+class AddExcerciseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
