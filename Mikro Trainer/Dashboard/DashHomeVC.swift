6b0a448cc317e0e38e834b7240c78cc941e9331a
Michael Gutkind <mikegutkind@gmail.com>
2019-01-05 22:01:38 -0500

    On to performance

--- Mikro Trainer/Dashboard/DashHomeVC.swift
+++ Mikro Trainer/Dashboard/DashHomeVC.swift
@@ -7,17 +7,39 @@
 //
 
 import UIKit
+import Charts
 
 class DashHomeVC: UIViewController {
 
     
+    @IBOutlet weak var lineChartView: LineChartView!
     //    pod 'Charts'
     override func viewDidLoad() {
         super.viewDidLoad()
+        setChartValues()
+        self.navigationItem.title = "Preformance"
 
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func setChartBtn(_ sender: Any) {
+        let count = Int(arc4random_uniform(20) + 3)
+        setChartValues(count: count)
+    }
+    
+    func setChartValues(count: Int=20){
+        let values = (0..<count).map { (i) -> ChartDataEntry in
+            let val = Double(arc4random_uniform(UInt32(count)) + 3)
+            return ChartDataEntry(x: Double(i), y: val)
+        }
+        
+        let set1 = LineChartDataSet(values: values, label: "DataSet 1")
+        let data = LineChartData(dataSet: set1)
+        
+        self.lineChartView.data = data
+        
+    }
 
 
 }
